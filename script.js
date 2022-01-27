ddd9ec2e08c60e6f31a761acfe7ae3b5a6293e38
zenon70 <97974128+zenon70@users.noreply.github.com>
2022-01-27 22:54:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,69 @@ $(document).ready(function(){
 	})
 });
 
+
+
+
+
+//Browser Compactibility
+
+
+browser = {};
+if (/(Edge\/[0-9]{2})/i.test(navigator.userAgent)) {
+    browser.agent = navigator.userAgent.match(/(Edge\/[0-9]{2})/i)[0].split("/")[0];
+    browser.version = parseInt(navigator.userAgent.match(/(Edge\/[0-9]{2})/i)[0].split("/")[1]);
+} else if (/(chrome\/[0-9]{2})/i.test(navigator.userAgent)) {
+    browser.agent = navigator.userAgent.match(/(chrome\/[0-9]{2})/i)[0].split("/")[0];
+    browser.version = parseInt(navigator.userAgent.match(/(chrome\/[0-9]{2})/i)[0].split("/")[1]);
+} else if (/(firefox\/[0-9]{2})/i.test(navigator.userAgent)) {
+    browser.agent = navigator.userAgent.match(/(firefox\/[0-9]{2})/i)[0].split("/")[0];
+    browser.version = parseInt(navigator.userAgent.match(/(firefox\/[0-9]{2})/i)[0].split("/")[1]);
+} else if (/(MSIE\ [0-9]{1})/i.test(navigator.userAgent)) {
+    browser.agent = navigator.userAgent.match(/(MSIE\ [0-9]{1})/i)[0].split(" ")[0];
+    browser.version = parseInt(navigator.userAgent.match(/(MSIE\ [0-9]+)/i)[0].split(" ")[1]);
+} else if (/(Opera\/[0-9]{1})/i.test(navigator.userAgent)) {
+    browser.agent = navigator.userAgent.match(/(Opera\/[0-9]{1})/i)[0].split("/")[0];
+    browser.version = parseInt(navigator.userAgent.match(/(Opera\/[0-9]{1})/i)[0].split("/")[1]);
+} else if (/(Trident\/[7]{1})/i.test(navigator.userAgent)) {
+    browser.agent = "MSIE";
+    browser.version = 11;
+} else {
+    browser.agent = false;
+    browser.version = false;
+}
+
+if (/(Windows\ NT\ [0-9]{1}\.[0-9]{1})/.test(navigator.userAgent)) {
+    browser.os = "Windows";
+
+    switch (parseFloat(navigator.userAgent.match(/(Windows\ NT\ [0-9]{1}\.[0-9]{1})/)[0].split(" ")[2])) {
+        case 6.0:
+            browser.osversion = "Vista";
+            break;
+        case 6.1:
+            browser.osversion = "7";
+            break;
+        case 6.2:
+            browser.osversion = "8";
+            break;
+        default:
+            browser.osversion = false;
+    }
+} else if (/(OS\ X\ [0-9]{2}\.[0-9]{1})/.test(navigator.userAgent)) {
+    browser.os = "OS X";
+    browser.osversion = navigator.userAgent.match(/(OS\ X\ [0-9]{2}\.[0-9]{1})/)[0].split(" ")[2];
+} else if (/(Linux)/.test(navigator.userAgent)) {
+    browser.os = "Linux";
+    browser.osversion = false;
+}
+
+if (browser.agent === "Chrome" || browser.agent === "chrome") {
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = "";
+    document.body.insertBefore(newDiv, document.body.firstChild);
+} else { //TODO: Remove for Prod only added to show some flexibility and testing 
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = "<b>" + browser.agent + "</b> is <i>not</i> supported. Only Google Chrome support our full functionality. Please download updated Chrome web browser.";
+    newDiv.setAttribute("style", "background-color:red;color:white;padding:12px;");
+    document.body.insertBefore(newDiv, document.body.firstChild);
+}
+
