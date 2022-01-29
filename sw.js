8dd883422fd6c4d3457af92eb38f6b1f7d7b509c
zenon70 <97974128+zenon70@users.noreply.github.com>
2022-01-30 01:48:02 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", e => {
+	e.waitUntil(
+		caches.open("static").then(cache => {
+			return cache.addAll(["./","./style.css","./script.js","./fevicon/android-chrome-192x192.png","./fevicon/android-chrome-512x512.png","./fevicon/apple-touch-icon.png","./fevicon/favicon-32x32.png","./fevicon/favicon-16x16.png","./fevicon/logo.png","https://eptonline.org/assets/achievements.mp3","https://eptonline.org/assets/wrongs.mp3"])
+		})
+
+		);
+});
+
+	self.addEventListener("fetch", e => {
+		console.log('Intercepting Fetch Request for: ${e.request.url}');
+	});
