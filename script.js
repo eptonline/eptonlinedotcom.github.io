c66f40008bc2503364a9844f6f680f145f507694
zenon70 <97974128+zenon70@users.noreply.github.com>
2022-01-30 01:47:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -165,42 +165,15 @@ if (OSName === "Android" ) {
 }
 
 
+//Code for SW
 
-//Counter
-
-
-
-incrementAndShowValue();
-function incrementAndShowValue() {
-  var value = getCookie("visitcounter") || 0;
-  var newValue = ("00000" + (Number(value) + 1)).slice(-6);
-  var container = document.getElementById("counterVisitor");
-  String(newValue).split("").forEach(function(item, index) {
-    container.children[index].innerHTML = item;
-  });
-  counter++;
-  setCookie("visitcounter", counter);
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js").then(registration => {
+    console.log("SW Registered!");
+    console.log(registration);
+  }).catch(error => {
+    console.log("SW Registration Failed!");
+    console.log(error);
+  })
 }
-
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + value + expires + "; path=https://eptonline.org/";
-}
-
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
 
