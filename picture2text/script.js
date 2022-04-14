5e03e7cc9d71f4d83544f55dace2229e3e0dbf55
zenon70 <97974128+zenon70@users.noreply.github.com>
2022-04-14 06:56:20 +0000

    Add files via upload

---
+++ picture2text/script.js
@@ -0,0 +1,44 @@
+const fileSelector = document.getElementById("fileSelector");
+const result = document.querySelector(".text");
+const loader = document.querySelector(".spinner");
+const img = document.getElementById("image");
+
+fileSelector.addEventListener("change", (event) => {
+  result.style.display = "none";
+  const file = event.target.files[0];
+  if (file) {
+    loader.style.display = "inline-block";
+    readImage(file);
+    recognize(file);
+  }
+});
+
+async function recognize(file) {
+  const {
+    data: { text },
+  } = await Tesseract.recognize(file, "eng");
+
+  loader.style.display = "none";
+  result.style.display = "flex";
+  result.innerHTML = text;
+}
+
+function readImage(file) {
+  const reader = new FileReader();
+  reader.addEventListener("load", (event) => {
+    img.src = event.target.result;
+    console.log(img);
+  });
+
+  reader.readAsDataURL(file);
+}
+
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
