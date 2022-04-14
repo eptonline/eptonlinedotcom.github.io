83d1e45ad80da1cb848639ef31a8bca537004d7c
zenon70 <97974128+zenon70@users.noreply.github.com>
2022-04-14 13:09:57 +0600

    Update scripts.js

--- realwords/Test01/scripts.js
+++ realwords/Test01/scripts.js
@@ -157,7 +157,7 @@ if(arr2.length===(arr2.length - final.length) && arr2.length===values.length){
 
 if(arr2.length!=(arr2.length - final.length) || arr2.length!=values.length){
   message001.innerHTML = "<text style= color:red >You Are Lost! Try Again.</text>" ;
-                disappear001.innerHTML = "<button onclick=window.alert(arr2)>Need Help? ℹ️</button>";
+                disappear001.innerHTML = "<button onclick=window.alert(arr2)>Need Help? 🛈</button>";
                 wrongs.play();
                 
                 reload1.innerHTML = "<div id=center001><button class=button001 onclick=reload()>Try Again</button></div>";
